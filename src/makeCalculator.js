18b6882dc0950607af709af3390a3710806c748b
Oksana Vasylyshyn <oksanamow@gmail.com>
2024-11-12 16:44:06 +0200

    Solution

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -4,7 +4,49 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  return {
+    // Початковий результат
+    result: 0,
+
+    // Додавання
+    add(value) {
+      this.result += value;
+    },
+
+    // Віднімання
+    subtract(value) {
+      this.result -= value;
+    },
+
+    // Множення
+    multiply(value) {
+      this.result *= value;
+    },
+
+    // Ділення
+    divide(value) {
+      if (value !== 0) {
+        this.result /= value;
+      } else {
+        // eslint-disable-next-line no-console
+        console.error('Cannot divide by zero');
+      }
+    },
+
+    // Операція, що приймає callback і значення
+    operate(operation, value) {
+      operation.call(this, value); // Виклик операції з переданим значенням
+
+      return this; // Повертаємо об'єкт для ланцюжкових викликів
+    },
+
+    // Скидання результату
+    reset() {
+      this.result = 0;
+
+      return this; // Повертаємо об'єкт для ланцюжкових викликів
+    },
+  };
 }
 
 module.exports = makeCalculator;
