fd92dbc286049294575af3025783efc5c4581939
OksanaW8 <85664858+OksanaW8@users.noreply.github.com>
2024-11-13 19:37:24 +0200

    Update makeCalculator.js

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -5,25 +5,25 @@
  */
 function makeCalculator() {
   return {
-    // Початковий результат
+   
     result: 0,
 
-    // Додавання
+   
     add(value) {
       this.result += value;
     },
 
-    // Віднімання
+   
     subtract(value) {
       this.result -= value;
     },
 
-    // Множення
+   
     multiply(value) {
       this.result *= value;
     },
 
-    // Ділення
+   
     divide(value) {
       if (value !== 0) {
         this.result /= value;
@@ -33,18 +33,16 @@ function makeCalculator() {
       }
     },
 
-    // Операція, що приймає callback і значення
+   
     operate(operation, value) {
-      operation.call(this, value); // Виклик операції з переданим значенням
-
-      return this; // Повертаємо об'єкт для ланцюжкових викликів
+      operation.call(this, value); 
+      return this; 
     },
 
-    // Скидання результату
     reset() {
       this.result = 0;
 
-      return this; // Повертаємо об'єкт для ланцюжкових викликів
+      return this; 
     },
   };
 }
